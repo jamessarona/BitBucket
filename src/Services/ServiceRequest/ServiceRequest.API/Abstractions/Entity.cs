26fec946f577cf70305425fd0b479ffce2381401
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 12:07:59 +0800

    feat: add common entities for models

---
+++ src/Services/ServiceRequest/ServiceRequest.API/Abstractions/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace BitBucket.Abstractions;
+
+public class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
---
+++ src/Services/ServiceRequest/ServiceRequest.API/Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace BitBucket.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
