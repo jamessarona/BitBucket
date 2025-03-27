a14392ec02c1ad081c58900791b3e956b62eec96
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 13:55:10 +0800

    feat: expose get service request by id

---
+++ src/Services/ServiceRequest/ServiceRequest.API/ServiceRequests/GetServiceRequestById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ServiceRequest.API.ServiceRequests.GetServiceRequestById;
+
+public record GetServiceRequestByIdResponse(Service ServiceRequest);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/servicerequest/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetServiceRequestByIdQuery(id));
+
+            var response = result.Adapt<GetServiceRequestByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetServiceRequestById")
+        .Produces<GetServiceRequestByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Read service request by id")
+        .WithDescription("Read service request by id");
+    }
+}
