a95d07875cfb1ea77f209df00237cee90c774303
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 14:33:20 +0800

    feat: expose service request post

--- src/Services/ServiceRequest/ServiceRequest.API/Models/Service.cs
+++ src/Services/ServiceRequest/ServiceRequest.API/Models/Service.cs
@@ -1,9 +1,13 @@
 ﻿namespace ServiceRequest.API.Models;
 
-public class Service : Entity<Guid>
+public class Service
 {
     public Guid Id { get; set; }
     public string BuildingCode { get; set; } = default!;
     public string Description { get; set; } = default!;
     public CurrentStatus CurrentStatus { get; set; } = CurrentStatus.Created;
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
 }
---
+++ src/Services/ServiceRequest/ServiceRequest.API/ServiceRequests/CreateServiceRequest/CreateServiceRequestEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ServiceRequest.API.ServiceRequests.CreateServiceRequest;
+
+public record CreateServiceRequest(string BuildingCode, string Description, string CreatedBy, string LastModifiedBy);
+public record CreateServiceRequestResponse(Guid Id);
+
+public class CreateServiceRequestEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/servicerequest",
+            async (CreateServiceRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateServiceRequestCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateServiceRequestResponse>();
+
+                return Results.Ok($"Created service request with {response.Id}");
+            })
+        .WithName("CreateServiceRequest")
+        .Produces<CreateServiceRequestResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Created service request")
+        .WithDescription("Created service request");
+    }
+}
