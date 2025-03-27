0a853d59fdb7fd895f3e560804cc1244eea59015
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 13:23:11 +0800

    feat: register services to container

--- src/Services/ServiceRequest/ServiceRequest.API/Program.cs
+++ src/Services/ServiceRequest/ServiceRequest.API/Program.cs
@@ -1,4 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
