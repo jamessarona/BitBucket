73e5cc75138c2503431ff405c245da716ded49d6
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 14:42:47 +0800

    feat: expose delete service request endpoint

---
+++ src/Services/ServiceRequest/ServiceRequest.API/ServiceRequests/DeleteServiceRequest/DeleteServiceRequestEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceRequest.API.ServiceRequests.DeleteServiceRequest;
+
+public record DeleteServiceRequestResponse(bool IsSuccess);
+
+public class DeleteServiceRequestEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/servicerequest/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteServiceRequestCommand(id));
+
+            var response = result.Adapt<DeleteServiceRequestResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteServiceRequest")
+        .Produces<DeleteServiceRequestResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete service request based on id")
+        .WithDescription("delete service request based on id");
+    }
+}
