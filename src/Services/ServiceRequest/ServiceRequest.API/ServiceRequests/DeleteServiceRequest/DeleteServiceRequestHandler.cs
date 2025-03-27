dcfda7540ad0aff4fa35360faaa06bdaec9a49b6
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 14:42:34 +0800

    feat: implement deletion of service

---
+++ src/Services/ServiceRequest/ServiceRequest.API/ServiceRequests/DeleteServiceRequest/DeleteServiceRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceRequest.API.ServiceRequests.DeleteServiceRequest;
+
+public record DeleteServiceRequestCommand(Guid Id) : ICommand<DeleteServiceRequestResult>;
+public record DeleteServiceRequestResult(bool IsSuccess);
+
+public class DeleteServiceRequestCommandValidator : AbstractValidator<DeleteServiceRequestCommand>
+{
+    public DeleteServiceRequestCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Service Request ID is required");
+    }
+}
+
+internal class DeleteServiceRequestCommandHandler
+    (IDocumentSession session)
+    : ICommandHandler<DeleteServiceRequestCommand, DeleteServiceRequestResult>
+{
+    public async Task<DeleteServiceRequestResult> Handle(DeleteServiceRequestCommand command, CancellationToken cancellationToken)
+    {
+        session.Delete<Service>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteServiceRequestResult(true);
+    }
+}
