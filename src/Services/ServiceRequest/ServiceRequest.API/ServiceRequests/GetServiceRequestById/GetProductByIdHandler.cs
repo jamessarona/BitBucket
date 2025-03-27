0f5b410487c7a5bd369ecc6e2acd99a38ea9bf7e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-27 13:54:53 +0800

    feat: add support of fetching service request by id

---
+++ src/Services/ServiceRequest/ServiceRequest.API/ServiceRequests/GetServiceRequestById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace ServiceRequest.API.ServiceRequests.GetServiceRequestById;
+
+public record GetServiceRequestByIdQuery(Guid Id) : IQuery<GetServiceRequestByIdResult>;
+public record GetServiceRequestByIdResult(Service ServiceRequest);
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session)
+    : IQueryHandler<GetServiceRequestByIdQuery, GetServiceRequestByIdResult>
+{
+    public async Task<GetServiceRequestByIdResult> Handle(GetServiceRequestByIdQuery query, CancellationToken cancellationToken)
+    {
+        var serviceRequest = await session.LoadAsync<Service>(query.Id, cancellationToken);
+        
+        if (serviceRequest is null)
+            throw new NotFoundException("Service Request Id not found");
+
+        return new GetServiceRequestByIdResult(serviceRequest);
+    }
+}
